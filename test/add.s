b42e9a58a9c083cb01b7b98d27ba82411ef29de7
David DiPaola <365233-DavidDiPaola@users.noreply.gitlab.com>
2020-11-14 12:57:26 -0800

    Implemented branch instructions.

--- test/add.s
+++ test/add.s
@@ -1,6 +1,10 @@
-/* x5 = 8 + 4 */
-li  x3, 8
-li  x4, 4
-add x5, x3, x4
-sw  x5, 0x00000030(x0)
-ebreak
+/* increment x3 from 0 to 9 */
+LOOP:
+	li  x3,  0
+	li  x4,  1
+	li  x5, 10
+	add x3, x3, x4
+	sw  x3, 0x00000030(x0)
+	blt x3, x5, LOOP
+
+	ebreak
