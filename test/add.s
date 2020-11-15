d38e19faa8f32ce2277d161bb04d1d6a93e83d3a
David DiPaola <365233-DavidDiPaola@users.noreply.gitlab.com>
2020-11-14 22:01:53 -0800

    Implemented jump-and-link instructions. Fixed PC off-by-one error when executing jump and branch instructions. Fixed J-type instruction decoding to use signed imm value.

--- test/add.s
+++ test/add.s
@@ -1,10 +1,21 @@
+START:
+	jal THINGY
+	ebreak
+
+.byte 0xFF, 0xFF, 0xFF, 0xFF
+
 /* increment x3 from 0 to 9 */
-LOOP:
+THINGY:
 	li  x3,  0
 	li  x4,  1
 	li  x5, 10
+1:
 	add x3, x3, x4
 	sw  x3, 0x00000030(x0)
-	blt x3, x5, LOOP
+	blt x3, x5, 1b
 
-	ebreak
+	ret
+
+.byte 0xFF, 0xFF, 0xFF, 0xFF
+
+	jal THINGY
